f4ed915b4d0c0da89ce3f831ce0d39c4b6d76a7f
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2020-10-29 19:31:05 +0300

    обновление пакетов

--- ui.packages/kit/src/layouts/Main/Header/index.js
+++ ui.packages/kit/src/layouts/Main/Header/index.js
@@ -2,33 +2,17 @@
 import { Menu } from '@ui.packages/kit';
 import { MenuContext } from '@ui.packages/contexts';
 
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 
 import cn from 'classnames';
 import styles from './default.module.scss';
 
 
 export default function Header() {
-  const [isOut, setOut] = useState(false);
+  const [isOut] = useState(false);
   const headerElement = useRef(null);
 
   const menuContext = useContext(MenuContext);
-
-  useEffect(() => {
-
-    function handleScrollEvent(event) {
-      event.preventDefault();
-      if (headerElement['current']) {
-        const rect = headerElement['current'].getBoundingClientRect();
-        setOut(rect['bottom'] <= 0);
-      }
-    }
-
-    document.querySelector('#scroll').addEventListener('scroll', handleScrollEvent);
-    return function cleanup() {
-      document.querySelector('#scroll').removeEventListener('scroll', handleScrollEvent);
-    };
-  });
 
   const wrapperClassName = cn(styles['wrapper'], {
     [styles['wrapper--fixed']]: isOut,
@@ -41,7 +25,7 @@ export default function Header() {
         <Menu menu={menuContext} />
       </div>
       <div className={styles['contacts']}>
-        <p className={styles['info']}>Как сязаться:</p>
+        <p className={styles['info']}>Как связаться:</p>
         <p className={styles['info']}><i className={cn(styles['icon'], "fas fa-phone")} /> +7 (978) 025 44 25</p>
         <p className={styles['info']}><i className={cn(styles['icon'], "far fa-envelope")} /> <a className={styles['link']} target="_blank" rel="noreferrer" href="mailto:sb-rk@mail.ru">sb-rk@mail.ru</a></p>
       </div>
